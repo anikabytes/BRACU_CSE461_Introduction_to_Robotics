c26dfbc23fda583ff6718d7214d381a1f9e35917
Anika Islam <poroma1298@gmail.com>
2023-12-12 00:40:22 +0600

    rectangle.py added

---
+++ Lab/Lab4/rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+import rospy
+from geometry_msgs.msg import Twist
+
+def rectangle():
+    # Starts a new node
+    rospy.init_node('robot_cleaner', anonymous=True)
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+
+    #Receiveing the user's input
+    print("Let's move robot in a rectangular path")
+    speed = input("Input speed:")
+    height = input("Enter height:")
+    width = input("Enter width: ")
+    
+    speed = float(speed)
+    height = float(height)
+    width = int(width)
+    
+
+   # move forward 
+    vel_msg.linear.x = -abs(speed)
+    velocity_publisher.publish(vel_msg)
+    rospy.sleep(width / speed)
+    
+    # stop after completing forward movement 
+    vel_msg.linear.x = 0
+    velocity_publisher.publish(vel_msg)
+        
+    # continue to move downward 
+    vel_msg.linear.y = -abs(speed)
+    velocity_publisher.publish(vel_msg)
+    rospy.sleep(height / speed)
+    
+    # stop after completing downward movement 
+    vel_msg.linear.y = 0
+    velocity_publisher.publish(vel_msg)    
+    
+    # continue to move forward 
+    vel_msg.linear.x = abs(speed)
+    velocity_publisher.publish(vel_msg)
+    rospy.sleep(width / speed)
+    
+    # stop after completing forward movement 
+    vel_msg.linear.x = 0
+    velocity_publisher.publish(vel_msg)
+    
+    # continue to move upward 
+    vel_msg.linear.y = abs(speed)
+    velocity_publisher.publish(vel_msg)
+    rospy.sleep(height / speed)
+    
+    # stop after completing upward movement 
+    vel_msg.linear.y = 0
+    velocity_publisher.publish(vel_msg)    
+    
+if __name__ == '__main__':
+  try:
+      #Testing our function
+      rectangle()
+  except rospy.ROSInterruptException: pass
